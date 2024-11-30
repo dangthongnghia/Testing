d7b0e8dfbe31206f178851b62c5f7fe490961a78
Đặng Thông Nghĩa <dangnghia2385@gmail.com>
2024-11-30 16:23:19 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import SellCar from "./pages/SellCar";
-import Login from "./pages/Login";
-import ContactInfo from "./components/ContactInfo";
-import Header from "./components/Header";
-import Register from "./pages/Register";
-import Footer from "./components/Footer";
+// src/App.js
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SellCar from './pages/SellCar';
+import Login from './pages/Login';
+import ContactInfo from './components/ContactInfo';
+import Header from './components/Header';
+import Register from './pages/Register';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sell" element={<SellCar />} />
+        <Route path="/sell-car" element={<SellCar />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/contact" element={<ContactInfo />} />
+        <Route path="/contact-info" element={<ContactInfo />} />
         <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
